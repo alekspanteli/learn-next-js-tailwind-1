12424f99889ed9e734da4e7cfb2b98876da67a0d
alekspanteli <alekspanteli@rambler.ru>
2023-01-13 09:23:11 +0000

    add shared layout

---
+++ components/layout.js
@@ -0,0 +1,4 @@
+export default function Layout({ children }) {
+    return <div className="max-w-4xl mx-auto px-3">{children}</div>;
+  }
+  
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,58 @@
 import Head from "next/head";
 import Link from "next/link";
+import Layout from "../components/layout";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Layout>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main>
-        <h1>
-          Read{" "}
-          <Link
-            href="/posts/first-post"
-            className="underline hover:no-underline"
-          >
-            this page!
-          </Link>
-        </h1>
+        <main>
+          <h1>
+            Read{" "}
+            <Link
+              href="/posts/first-post"
+              className="underline hover:no-underline"
+            >
+              this page!
+            </Link>
+          </h1>
 
-        <p>
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          <p>
+            Get started by editing <code>pages/index.js</code>
+          </p>
 
-        <div>
-          <a href="https://nextjs.org/docs">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <div>
+            <a href="https://nextjs.org/docs">
+              <h3>Documentation &rarr;</h3>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </a>
 
-          <a href="https://nextjs.org/learn">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            <a href="https://nextjs.org/learn">
+              <h3>Learn &rarr;</h3>
+              <p>Learn about Next.js in an interactive course with quizzes!</p>
+            </a>
 
-          <a href="https://github.com/vercel/next.js/tree/master/examples">
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <a href="https://github.com/vercel/next.js/tree/master/examples">
+              <h3>Examples &rarr;</h3>
+              <p>Discover and deploy boilerplate example Next.js projects.</p>
+            </a>
 
-          <a href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+            <a href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
+              <h3>Deploy &rarr;</h3>
+              <p>
+                Instantly deploy your Next.js site to a public URL with Vercel.
+              </p>
+            </a>
+          </div>
+        </main>
 
-      <footer>Powered by Vercel</footer>
+        <footer>Powered by Vercel</footer>
+      </Layout>
     </div>
   );
 }
