6aca9eab580323ad06c48107d811cada1f3a265b
alekspanteli <alekspanteli@rambler.ru>
2023-01-13 09:08:43 +0000

    add first post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,11 +1,24 @@
-import Link from 'next/link';
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
       <h2>
-        <Link href="/">Baasdck to home</Link>
+        <Link href="/" className="underline hover:no-underline">
+          Back to home
+        </Link>
       </h2>
     </>
   );
